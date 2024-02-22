09ffeb50b029e6bb63e9eae46c84f700f5750e69
Kshitij Magare <88279452+KshitijMagare19@users.noreply.github.com>
2024-02-23 00:27:18 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Buy and Sell a Share at most twice/buy-and-sell-a-share-at-most-twice.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+        
+        return a;
+    }
+    
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+    
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine().trim());
+        while(t-- > 0){
+            
+            int n; 
+            n = Integer.parseInt(br.readLine().trim());
+            
+            
+            int[] price = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            int res = obj.maxProfit(n, price);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int maxProfit(int n, int[] price) {
+        int curr[]=new int[5];
+        int next[]=new int[5];
+        for(int ind=n-1;ind>=0;ind--){
+            for(int trans=3;trans>=0;trans--){
+                int profit=0;
+                //buy
+                if(trans%2==0){
+                    profit=Math.max(-price[ind]+next[trans+1],next[trans]);
+                }//sell
+                else{
+                    profit=Math.max(price[ind]+next[trans+1],next[trans]);
+                }
+                curr[trans]=profit;        
+            }
+            next=curr;
+        }
+        return curr[0];
+    }
+}
+        
